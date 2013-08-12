aba8784669d7b0a510939d79c2f4ec79a0bfeb0f
Leon Ng <lnhr_8544@hotmail.com>
2013-08-13 03:13:07 +0800

    Revert "Revert "latest changes with inprogress events tab, social media(viewcomments done),likes and commenting not yet done.""
    This reverts commit ee0b5430e8e19d73b7b30d369387b0414bf26e31.

---
+++ gigReplayApp/HomeViewCustomCell2.m
@@ -0,0 +1,30 @@
+//
+//  HomeViewCustomCell2.m
+//  gigReplay
+//
+//  Created by Leon Ng on 6/8/13.
+//  Copyright (c) 2013 Leon Ng. All rights reserved.
+//
+
+#import "HomeViewCustomCell2.h"
+
+@implementation HomeViewCustomCell2
+@synthesize fbProfileImageView,videoImageView,userName;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
